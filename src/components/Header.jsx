e8ecfda74d1b15e19de00914e214a99b50e8e03b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-06 15:12:57 -0300

    título do quadro e edição do título implementados

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+export default class Header extends Component {
+  state = {
+    boardName: 'Meu Quadro',
+    editingBoardName: false,
+  };
+
+  handleBoardNameClick = () => this.setState({ editingBoardName: true });
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleInputEnter = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editingBoardName: false });
+    }
+  }
+
+  render() {
+    const { boardName, editingBoardName } = this.state;
+    const boardNameInputStyle = { width: `${boardName.length + 2.5}ch` };
+    return (
+      <header className="Header">
+        { editingBoardName ? (
+          <input
+            value={ boardName }
+            type="text"
+            style={ boardNameInputStyle }
+            onKeyDown={ this.handleInputEnter }
+            onChange={ this.handleInputChange }
+            name="boardName"
+            className="Header__board-input"
+          />
+        ) : (
+          <h1
+            onClick={ this.handleBoardNameClick }
+            className="Header__board-title"
+          >
+            { boardName }
+          </h1>
+        ) }
+      </header>
+    );
+  }
+}
