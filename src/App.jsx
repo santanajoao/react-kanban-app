3bb81b1e8f74aceeb2e1479001499563d8fd7a85
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-06 14:25:01 -0300

    redux e react-redux instalados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './styles/App.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div className="App">
+        <Header />
+      </div>
     );
   }
 }
