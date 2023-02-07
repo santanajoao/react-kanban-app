e185054be442ce8cea2407ef14786355b8684ab6
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-07 09:52:19 -0300

    estrutura e estilo do card que adiciona uma nova coluna implementado

--- src/components/AddColumnCard.jsx
+++ src/components/AddColumnCard.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BsPlusLg } from 'react-icons/bs';
+import { GrFormClose } from 'react-icons/gr';
+import '../styles/AddColumnCard.css';
 
 export default class AddColumnCard extends Component {
   state = {
@@ -15,18 +18,38 @@ export default class AddColumnCard extends Component {
     return (
       <div className="AddColumnCard">
         { isAdding ? (
-          <>
+          <form className="AddColumnCard__form">
             <input
               type="text"
               placeholder="Insira o título da coluna"
               name="newColumnTitle"
+              className="AddColumnCard__title-input"
+              autoFocus
             />
-            <button type="button">Adicionar coluna</button>
-            <button type="button" onClick={ this.handleAddingStatus }>X</button>
-          </>
+            <div className="AddColumnCard__btns-wrapper">
+              <button
+                className="AddColumnCard__submit-btn"
+                type="button"
+              >
+                Adicionar coluna
+              </button>
+              <button
+                type="button"
+                onClick={ this.handleAddingStatus }
+                className="AddColumnCard__close-form-btn"
+              >
+                <GrFormClose className="close-form-icon" />
+              </button>
+            </div>
+          </form>
           ) : (
-          <button type="button" onClick={ this.handleAddingStatus }>
-            Adicionar outra lista
+          <button
+            type="button"
+            onClick={ this.handleAddingStatus }
+            className="AddColumnCard__open-form-btn"
+          >
+            <BsPlusLg className="open-form-icon" />
+            Adicionar uma coluna
           </button>
         ) }
       </div>
