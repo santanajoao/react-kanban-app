9b7d9bc2f347445f3ea33435721444708692215e
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-11 11:53:06 -0300

    Refatorações no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,11 @@ import EditableTitle from './EditableTitle';
 import '../styles/Header.css';
 
 export default class Header extends Component {
-  state = {
-    title: 'Meu quadro',
-  };
-
-  handleTitleChange = (event) => {
-    const { value } = event.target;
-    this.setState({ title: value });
-  };
-
   render() {
-    const { title } = this.state;
     return (
       <header className="Header">
         <EditableTitle
-          handleTitleChange={ this.handleTitleChange }
-          title={ title }
+          title="Meu quadro"
           blockClassName="Header"
         />
       </header>
