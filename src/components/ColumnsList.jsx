c21dd47c6206d77ce11462e6ce43b1b38833e1f5
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-17 23:22:48 -0300

    refactor: change ColumnsList into a function component

--- src/components/ColumnsList.jsx
+++ src/components/ColumnsList.jsx
@@ -1,32 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import BoardColumn from './BoardColumn';
 import AddBoardColumn from './AddBoardColumn';
 import '../styles/ColumnsList.css';
 
-class ColumnsList extends Component {
-  render() {
-    const { columns } = this.props;
-    const { length } = columns;
-    return (
-      <ol className="ColumnsList">
-        {columns.map(({ id, title, cards }, index) => (
-          <li key={id}>
-            <BoardColumn
-              title={title}
-              cards={cards}
-              index={index}
-              length={length}
-            />
-          </li>
-        ))}
-        <li>
-          <AddBoardColumn />
+export default function ColumnsList() {
+  const { columns } = useSelector((state) => state.kanban);
+  const { length } = columns;
+  return (
+    <ol className="ColumnsList">
+      {columns.map(({ id, title, cards }, index) => (
+        <li key={id}>
+          <BoardColumn
+            title={title}
+            cards={cards}
+            index={index}
+            length={length}
+          />
         </li>
-      </ol>
-    );
-  }
+      ))}
+      <li>
+        <AddBoardColumn />
+      </li>
+    </ol>
+  );
 }
 
 ColumnsList.propTypes = {
@@ -38,9 +35,3 @@ ColumnsList.propTypes = {
     })
   ).isRequired,
 };
-
-const mapStateToProps = ({ kanban }) => ({
-  columns: kanban.columns,
-});
-
-export default connect(mapStateToProps)(ColumnsList);
