9335ff70ff97c8a15bec5a044e37d64cb55e60d3
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-17 23:20:08 -0300

    refactor: change Header into a function component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
 import EditableTitle from './EditableTitle';
 import '../styles/Header.css';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header className="Header">
-        <EditableTitle title="Meu quadro" blockClassName="Header" />
-      </header>
-    );
-  }
+export default function Header() {
+  return (
+    <header className="Header">
+      <EditableTitle title="Meu quadro" blockClassName="Header" />
+    </header>
+  );
 }
