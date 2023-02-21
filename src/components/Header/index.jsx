4e52cc8626707dd1db0cf2876c3b8252bb400928
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-21 12:30:16 -0300

    feat: save board name on local storage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,9 +2,19 @@ import EditableText from '../EditableText';
 import styles from './style.module.css';
 
 export default function Header() {
+  const boardTitle = JSON.parse(localStorage.getItem('boardName'));
+
+  function handleEnter(value) {
+    localStorage.setItem('boardName', JSON.stringify(value));
+  }
+
   return (
     <header className={styles.header}>
-      <EditableText title="Meu quadro" styles={styles} />
+      <EditableText
+        title={boardTitle || 'Meu quadro'}
+        styles={styles}
+        onEnter={handleEnter}
+      />
     </header>
   );
 }
